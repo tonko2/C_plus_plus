504b9c2376ab7cd4df62138d1d88e46d977c9be4
Yuya <tonkotsu@wlan3264.u-aizu.ac.jp>
2014-04-30 17:29:58 +0900

    Create directry and rearrange

---
+++ Math/Extgcd.cpp
@@ -0,0 +1,15 @@
+
+ax + by = gcd(a,b) のxとyを求める
+   
+int extgcd(int a, int b, int &x, int &y){
+   int d = a;
+   if(b != 0){
+      d = extgcd(b,a%b,y,x);
+      y -= (a/b) * x;
+   } else {
+      x = 1, y = 0;
+   }
+   return d; // b == 0 then return a
+}
+
+   
