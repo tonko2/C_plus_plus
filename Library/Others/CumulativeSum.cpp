5adcb6320f6286b26df4392f52f16fe287379a28
tonko2 <10cherry07@gmail.com>
2014-11-25 12:51:51 +0900

    add new CumulativeSum.cpp

--- Library/Others/CumulativeSum.cpp
+++ Library/Others/CumulativeSum.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int INF = 1<<29;
 
 int main(){
-   int n;
+   int n,m;
    int a[101][101];
-   int sum[102][102];
-  
-   while(cin >> n){
-      fill(sum[0],sum[101],0);
-      int ans = -INF;
-      for(int i=1; i<=n; i++)
-         for(int j=1; j<=n; j++) cin >> a[i][j];
+   cin >> n >> m;
+   
+   for(int i=0; i<n; i++)
+	  for(int j=0; j<m; j++) cin >> a[i][j];
 
-      for(int i=1; i<=n; i++){
-         for(int j=1; j<=n; j++){
-            sum[i][j] = a[i][j] + sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1];
-         }
-      }
+   int acum[101][101];
+   memset(acum,0,sizeof(acum));
 
-      for(int i=1; i<=n; i++){
-         for(int j=1; j<=n; j++){
-            for(int k=1; k<=n; k++){
-               for(int l=1; l<=n; l++){
-                  if(i <= k && j <= l){
-                     int value = sum[k][l] - sum[k][j-1] - sum[i-1][l] + sum[i-1][j-1];
-                     ans = max(ans,value);
-                  }
-                 
-               }
-            }
-         }
-      }
 
-     
-      cout << ans << endl;
+   for(int i=1; i<=n; i++){
+	  for(int j=1; j<=m; j++){
+		 int x = a[i-1][j-1];
+		 acum[i][j] = x + acum[i-1][j] + acum[i][j-1] - acum[i-1][j-1];
+	  }
    }
+
+
+   int ans = 0;
+
+   for(int i=1; i<=n; i++){
+	  for(int j=1; j<=m; j++){
+		 for(int k=i; k<=n; k++){
+			for(int l=j; l<=m; l++){
+			   int sum = acum[k][l] - acum[i-1][l] - acum[k][j-1] + acum[i-1][j-1];
+			   ans = max(ans,sum);
+			}
+		 }
+	  }
+   }
+
+   cout << ans << endl;
   
    return 0;
 }
