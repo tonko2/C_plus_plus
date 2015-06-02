c26453baa5731c1a505c97ec9648e9f1bb24a369
tonko2 <10cherry07@gmail.com>
2015-06-02 20:03:23 +0900

    revise modpow

--- Library/Others/ModPow.cpp
+++ Library/Others/ModPow.cpp
@@ -8,15 +8,65 @@ ll mod_pow(ll x, ll n, ll mod){
 }
 
 //繰り返し２乗法 (行列)
-vector<int> multi(vector<int>& matrix, int k){
-   if(k == 1) return matrix;
+typedef vector<vector<ll> > Matrix;
 
-   vector<int> tmp = multi(matrix,k/2);
-   if(k % 2){
-      // 行列 * (行列*行列)という繰り返し２乗法
-      return calc(matrix,calc(tmp,tmp));
-   } else {
-      // 繰り返し２乗法
-      return calc(tmp,tmp);
+Matrix calc(Matrix a, Matrix b){
+   int N = a.size();
+   Matrix res(N,vector<ll>(N)); //最初に0で初期化されている
+   
+   for(int i=0; i<N; i++){
+	  for(int j=0; j<N; j++){
+		 for(int k=0; k<N; k++){
+			res[i][j] += a[i][k] * b[k][j];
+		 }
+	  }
    }
+   return res;
+}
+     
+Matrix modpow(Matrix g, int n){
+
+   
+   if(n == 0){
+	  Matrix unit(g.size(),vector<ll>(g.size()));
+	  for(int i=0; i<g.size(); i++) unit[i][i] = 1;
+	  return unit;
+   }
+     
+   Matrix res = modpow(g,n/2);
+   if(n & 1) return calc(g,calc(res,res)); 
+   else return calc(res,res);
+       
+}
+
+
+void display(Matrix a){
+   int N = a.size();
+   for(int i=0; i<N; i++){
+	  for(int j=0; j<N; j++){
+		 cout << a[i][j] << " ";
+	  }
+	  cout << endl;
+   }
+}
+     
+int main(){
+     
+   Matrix a(3,vector<ll>(3));
+   
+   for(int i=0; i<3; i++)
+	  for(int j=0; j<3; j++) a[i][j] = i*3 + (j+1);
+
+   Matrix b = calc(a,a);
+   Matrix c = calc(b,b);
+
+   display(modpow(a,1));
+   display(modpow(a,2));
+   display(modpow(a,3));
+   display(modpow(a,4));
+   
+   display(c);
+     
+       
+   return 0;
 }
